e6b1d95bb5ffaef10230734a89b074c8168ecb23
GaaCBot <noreply@gaac.vercel.app>
2022-12-27 22:01:43 +0200

    Uploaded sw.js via GAAC

---
+++ uploads/sw.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', function(event) {
+  // Perform install steps
+});
+
+self.addEventListener('activate', function(event) {
+  // Perform activation steps
+});
+
+self.addEventListener('fetch', function(event) {
+  // Perform fetch event handling
+});
